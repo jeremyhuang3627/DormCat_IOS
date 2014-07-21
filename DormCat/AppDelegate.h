bd3ac6f81182660c250f108be978d9ab73acb166
Jeremy Jie Huang <jh3627@stern.nyu.edu>
2014-07-21 09:09:23 -0400

    Initial Commit

---
+++ DormCat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DormCat
+//
+//  Created by Huang Jie on 7/21/14.
+//  Copyright (c) 2014 Huang Jie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
